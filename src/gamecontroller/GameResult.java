4920d17f521ca25d35135a44059967a533b54e6f
Jeon(Tany) <79444040+juni8453@users.noreply.github.com>
2021-11-26 19:49:26 +0900

    게임의 결과를 판단하고 출력할 GameResult 클래스를 만들었습니다.
    Main 클래스에서 플레이어가 숫자를 입력하고 GameResult 클래스의 메소드를 활용하여 게임을 진행하도록 구현했습니다.
    
    GameResult 클래스에는 일단 스트라이크, 볼, 낫씽일 때의 메소드를 따로 만들어서 활용할 예정입니다.

---
+++ src/gamecontroller/GameResult.java
@@ -0,0 +1,21 @@
+package gamecontroller;
+
+// 게임 결과를 담당하는 클래스
+public class GameResult {
+
+    public void gameResult() {
+
+    }
+
+    public void Strike() {
+
+    }
+
+    public void Ball() {
+
+    }
+
+    public void nothing() {
+
+    }
+}
