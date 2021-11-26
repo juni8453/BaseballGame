ec45b2b2b4dade8c3b23fa286644d7368858dc34
TanyJeon <juni8453@naver.com>
2021-11-26 20:08:41 +0900

    난수를 담는 리스트 List<String>을 List<Integer>로 변경했습니다.

--- src/gamecontroller/Enemy.java
+++ src/gamecontroller/Enemy.java
@@ -7,8 +7,8 @@ import java.util.List;
 public class Enemy {
 
     // 컴퓨터가 뽑은 숫자를 랜덤하게 섞는다.
-    public List<String> shuffleNumbers() {
-        List<String> numbers = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9");
+    public List<Integer> shuffleNumbers() {
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9);
         Collections.shuffle(numbers);
             return numbers;
     }
