5d52e00a05fc7f481422c844e065df58fa75bc11
TanyJeon <juni8453@naver.com>
2021-11-26 19:36:33 +0900

    게임의 결과를 판단하고 출력할 GameResult 클래스를 만들었습니다.
    Main 클래스에서 플레이어가 숫자를 입력하고 GameResult 클래스의 메소드를 활용하여 게임을 진행하도록 구현했습니다.
    
    GameResult 클래스에는 일단 스트라이크, 볼, 낫씽일 때의 메소드를 따로 만들어서 활용할 예정입니다.

--- src/gamecontroller/Main.java
+++ src/gamecontroller/Main.java
@@ -8,10 +8,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         GameSetting setting = new GameSetting();
+        GameResult result = new GameResult();
 
         System.out.println("================");
         System.out.println("야구게임을 시작합니다.");
+
+        // 섞은 난수를 shuffleNumbers 변수에 저장한다.
         List<String> shuffleNumbers = setting.gameSetting();
-        System.out.println(shuffleNumbers);
+
+        // 플레이어는 세자리 숫자를 입력한다.
+        System.out.print("숫자를 입력해주세요 ! 예) 123 ");
+        int playerPickNumbers = sc.nextInt();
+        result.gameResult();
     }
 }
