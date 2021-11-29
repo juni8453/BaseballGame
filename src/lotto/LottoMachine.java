2f81176a6b66efda32844a5f57a6c0e465d0dbfb
TanyJeon <juni8453@naver.com>
2021-11-29 17:00:03 +0900

    LottoMachine 클래스에 메소드를 추가헀습니다.
    추가한 메소드는,
    1 ~ 45 번호를 리스트에 넣어주는 settingNumbers() 메소드
    리스트의 번호를 섞어주는 shuffleNumbers() 메소드
    섞은 번호를 받아 앞에서부터 6개의 번호를 뽑아주는 pickNumbers() 메소드
    5번 반복 후 프로그램을 종료시키는 stopMachine() 메소드 입니다.

--- src/lotto/LottoMachine.java
+++ src/lotto/LottoMachine.java
@@ -4,6 +4,8 @@ public class LottoMachine {
     public static void main(String[] args) {
 
         LottoSetting setting = new LottoSetting();
-
+        System.out.println("로또번호 자동 추첨을 시작합니다.");
+        System.out.println("=========================");
+        setting.settingNumbers();
     }
 }
--- src/lotto/LottoSetting.java
+++ src/lotto/LottoSetting.java
@@ -1,17 +1,47 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class LottoSetting {
 
-    public void setNumbers() {
+    private int count = 0;
+
+    public void settingNumbers() {
         List<Integer> lottoNumbers = new ArrayList<>();
         for (int i = 0; i < 46; i++) {
             lottoNumbers.add(i);
         }
 
         lottoNumbers.remove(0);
+        shuffleNumbers(lottoNumbers);
+    }
 
+    public void shuffleNumbers(List<Integer> lottoNumbers) {
+        Collections.shuffle(lottoNumbers);
+        pickNumbers(lottoNumbers);
+    }
+
+    public void pickNumbers(List<Integer> lottoNumbers) {
+        if (count < 5) {
+            lottoNumbers.stream().limit(6).sorted(Comparator.naturalOrder())
+                    .forEach(s -> System.out.print(s + "번" + " "));
+            System.out.println();
+            count += 1;
+            settingNumbers();
+        }
+
+        if (count == 5) {
+            stopMachine();
+        }
+    }
+
+
+    public void stopMachine() {
+        System.out.println("=========================");
+        System.out.println("자동 추첨을 완료했습니다.");
+        System.exit(0);
     }
 }
