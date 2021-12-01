fd0bc09ac6298f94d6a3664e7c13953e02e65d81
tany <juni8453@naver.com>
2021-12-01 12:44:03 +0900

    gamemanager package에서 Run 클래스를 추가했습니다.
    Setting 클래스에서 플레이어가 선택한 직업에 따라 각 영웅 인스턴스를 생성하도록 구현했습니다.
    
    if가 4개나 쓰여서 합칠 수 있는 방법이 없을까 고민하고 있는데, 추후에 방법이 생각나면
    
    수정할 예정입니다.. !

--- src/simplerpg/gamemanager/Run.java
+++ src/simplerpg/gamemanager/Run.java
@@ -5,20 +5,9 @@ import simplerpg.hero.Mage;
 import simplerpg.hero.Thief;
 import simplerpg.hero.Warrior;
 
-import java.util.Scanner;
+public class Run {
 
-public class GameSetting {
-    Scanner sc = new Scanner(System.in);
-    Run run = new Run();
-
-    public void choiceHero() {
-        System.out.println("직업을 선택하십시오.");
-        System.out.println("[1]전사 [2]궁수 [3]마법사 [4]도적");
-        int choiceHeroNumber = sc.nextInt();
-
-        System.out.println("영웅의 이름을 입력해주세요.");
-        String heroName = sc.next();
-
+    public void startGame(int choiceHeroNumber, String heroName) {
         if (choiceHeroNumber == 1) {
             Warrior warrior = new Warrior(heroName);
         }
@@ -34,8 +23,5 @@ public class GameSetting {
         if (choiceHeroNumber == 4) {
             Thief thief = new Thief(heroName);
         }
-
-        // 게임 시작 !
-        run.startGame(choiceHeroNumber, heroName);
     }
 }
