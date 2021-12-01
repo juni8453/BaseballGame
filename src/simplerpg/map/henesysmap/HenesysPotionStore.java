566a4d6510a381597e6b68e3879648a512463186
tany <juni8453@naver.com>
2021-12-01 15:10:43 +0900

    4가지의 마을, 그 마을에서의 사냥터, 상점 등을 구현하고 있습니다.
    영웅 클래스를 고르고 게임이 시작되면 게임이 진행하는 동안 이동할 맵을 구현하려고 합니다.
    
    그리고 각 클래스에 맞는 마을이 지정되어 시작할 때 그 마을에서 시작할 수 있도록 구현하려고 합니다 !

--- src/simplerpg/map/henesysmap/HenesysPotionStore.java
+++
@@ -1,6 +0,0 @@
-package simplerpg.map.henesysmap;
-
-public class HenesysPotionStore {
-    private int hpPotion = 5;
-    private int mpPotion = 5;
-}
--- src/simplerpg/map/henesysmap/HuntingGround.java
+++
@@ -1,5 +0,0 @@
-package simplerpg.map.henesysmap;
-
-public class HuntingGround {
-
-}
